b6563b3fc7ae6dcafce84ddf3df49c346fe08392
Farhad Fouladi <fouladi@gmx.de>
2024-07-02 13:47:29 +0200

    fix: color scheme gruvbox setup

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -4,8 +4,35 @@ return {
         lazy = false,
         name = "gruvbox",
         priority = 1000,
-        palette_overrides = { bright_green = "#b8bb26" },
         config = function()
+            require("gruvbox").setup({
+                contrast = "soft",
+                palette_overrides = { bright_green = "#b8bb26" },
+                overrides = {
+                    Comment = { fg = "#81878f", italic = true, bold = false },
+                    -- fold
+                    Folded = { fg = "#fe8019", bg = "#0E1018", italic = true },
+                    -- new git colors
+                    DiffAdd = {
+                        bold = true,
+                        reverse = false,
+                        fg = "",
+                        bg = "#2a4333",
+                    },
+                    DiffChange = {
+                        bold = true,
+                        reverse = false,
+                        fg = "",
+                        bg = "#333841",
+                    },
+                    DiffDelete = {
+                        bold = true,
+                        reverse = false,
+                        fg = "#442d30",
+                        bg = "#442d30",
+                    },
+                },
+            })
             vim.cmd.colorscheme("gruvbox") -- Comment in order to deactivate "gruvbox"!
         end,
     },
