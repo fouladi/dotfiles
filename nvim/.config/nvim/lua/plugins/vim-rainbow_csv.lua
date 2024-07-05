4de14d42ba06ef93e4dd645b0f19cf04df2da703
Farhad Fouladi <fouladi@gmx.de>
2024-07-05 15:41:33 +0200

    doc: more comment for a csv-rainbow plugin

--- nvim/.config/nvim/lua/plugins/vim-rainbow_csv.lua
+++ nvim/.config/nvim/lua/plugins/vim-rainbow_csv.lua
@@ -1,3 +1,12 @@
 return {
     "mechatroner/rainbow_csv",
+-- |Filetype       | Separator     | Extension | Properties                                          |
+-- |---------------|---------------|-----------|-----------------------------------------------------|
+-- |csv            | , (comma)     | .csv      | Ignored inside double-quoted fields                 |
+-- |tsv            | \t (TAB)      | .tsv .tab |                                                     |
+-- |csv_semicolon  | ; (semicolon) |           | Ignored inside double-quoted fields                 |
+-- |csv_whitespace | whitespace    |           | Consecutive whitespaces are merged                  |
+-- |csv_pipe       | |      (pipe) |           |                                                     |
+-- |rfc_csv        | , (comma)     |           | Same as "csv" but allows multiline fields           |
+-- |rfc_semicolon  | ; (semicolon) |           | Same as "csv_semicolon" but allows multiline fields |
 }
