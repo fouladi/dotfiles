40313d5491dd8cee8e3cbee6d23685ccba35c3c2
Farhad Fouladi <fouladi@gmx.de>
2024-07-10 15:53:12 +0200

    feat: renamed plugin

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -7,4 +7,8 @@ return {
 
         "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+        require("markview").setup({})
+        vim.api.nvim_set_keymap("n", "<leader>md", ":Markview<CR>", { silent = true })
+    end,
 }
