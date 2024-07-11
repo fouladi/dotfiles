9d418bb74e143e721c96b6498a59fc6b0c219092
Farhad Fouladi <fouladi@gmx.de>
2024-07-11 08:43:11 +0200

    fix: unnecessary import module removed

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,10 +1,6 @@
 return {
     "OXY2DEV/markview.nvim",
     dependencies = {
-        -- You may not need this if you don't lazy load
-        -- Or if the parsers are in your $RUNTIMEPATH
-        "nvim-treesitter/nvim-treesitter",
-
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
