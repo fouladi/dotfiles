23d618a262d9c4c1c942e09901d5431750367d4c
Farhad Fouladi <fouladi@gmx.de>
2024-10-04 13:20:31 +0200

    fix: back to tpope surrond again

--- nvim/.config/nvim/lua/plugins/vim-surround.lua
+++ nvim/.config/nvim/lua/plugins/vim-surround.lua
@@ -12,16 +12,8 @@
 --     *make strings               ys$"            "make strings"
 --     [delete ar*ound me!]        ds]             delete around me!
 --     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     surr*ound_words             csw"            "surround_words"
 --     'change quot*es'            cs'"            "change quotes"
---     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
---     delete(functi*on calls)     dsf             function calls
 return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end,
+    "tpope/vim-surround",
 }
