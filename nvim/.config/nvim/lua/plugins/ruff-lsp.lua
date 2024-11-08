cd40d813fe8aaa2e7d4c4f466c5a188069f488c0
Farhad Fouladi <fouladi@gmx.de>
2024-11-08 09:45:22 +0100

    fix: deprecated ruff-lsp changed to ruff

--- nvim/.config/nvim/lua/plugins/ruff-lsp.lua
+++ nvim/.config/nvim/lua/plugins/ruff-lsp.lua
@@ -1,6 +1,6 @@
 return {
     "astral-sh/ruff-lsp",
-    require("lspconfig").ruff_lsp.setup({
+    require("lspconfig").ruff.setup({
         init_options = {
             settings = {
                 -- Any extra CLI arguments for `ruff` go here.
