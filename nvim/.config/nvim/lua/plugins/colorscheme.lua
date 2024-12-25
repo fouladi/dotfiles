402225395d4021dce5e7edf64c479918f83184fa
Farhad Fouladi <fouladi@gmx.de>
2024-12-25 11:47:26 +0100

    feat: adding trouble pluging to nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,45 +1,80 @@
 return {
     {
-        "ellisonleao/gruvbox.nvim",
-        lazy = false,
-        name = "gruvbox",
-        priority = 1000,
-        config = function()
-            require("gruvbox").setup({
-                contrast = "dark",
-                palette_overrides = { bright_green = "#b8bb26" },
-                overrides = {
-                    Comment = { fg = "#81878f", italic = true, bold = false },
-                    -- fold
-                    Folded = { fg = "#fe8019", bg = "#0E1018", italic = true },
-                    -- new git colors
-                    DiffAdd = {
-                        bold = true,
-                        reverse = false,
-                        fg = "",
-                        bg = "#2a4333",
-                    },
-                    DiffChange = {
-                        bold = true,
-                        reverse = false,
-                        fg = "",
-                        bg = "#333841",
-                    },
-                    DiffDelete = {
-                        bold = true,
-                        reverse = false,
-                        fg = "#442d30",
-                        bg = "#442d30",
-                    },
-                    DiffText = {
-                        bold = true,
-                        reverse = true,
-                        fg = "",
-                        bg = "#213352",
-                    },
-                },
+        "sainnhe/gruvbox-material",
+        opacity = 0.92,
+        init = function(spec)
+            vim.g.gruvbox_material_background = "medium" -- soft|medium|hard
+            vim.g.gruvbox_material_foreground = "original" -- material|mix|original
+            vim.g.gruvbox_material_ui_contrast = "low" -- low|high
+            vim.g.gruvbox_material_better_performance = 1
+            vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+            vim.g.gruvbox_material_inlay_hints_background = "dimmed"
+
+            local name = vim.fs.basename(spec[1])
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                desc = "User: Highlights for " .. name,
+                pattern = name,
+                callback = function()
+                    local setHl = function(...)
+                        vim.api.nvim_set_hl(0, ...)
+                    end
+                    local hlCmd = vim.cmd.highlight
+
+                    -- NOTE: MISSING HIGHLIGHTS
+                    -- stop globals like `vim` in lua or `Objc` in JXA from being overwritten
+                    setHl("@lsp.type.variable", {})
+                    -- placeholders like the `%s` in `string.format("foo %s bar")`
+                    setHl("@character.printf", { link = "Purple" })
+
+                    -- NOTE: selection hardly visible in DressingInput
+                    -- (keep it for the scrollbar though)
+                    setHl("Visual", { bg = "#385055" })
+                    setHl("SatelliteBar", { bg = "#45403d" })
+
+                    -- General
+                    setHl("TSParameter", { fg = "#679bbf" })
+                    setHl("TSConstant", { fg = "#948ecb" })
+                    setHl("@string.documentation.python", { link = "Comment" })
+                    setHl("@keyword.return", { bold = true, fg = "#f6843a" })
+                    setHl("TSPunctBracket", { fg = "#af7e5d" })
+                    vim.defer_fn(function()
+                        setHl("@constructor.lua", { fg = "#9b97a8" })
+                    end, 1)
+
+                    -- md `inline` code in comments
+                    setHl("@markup.raw", { fg = "#a9b665", bg = "#3c3836" })
+
+                    -- cursorword
+                    setHl("LspReferenceWrite", { underdashed = true })
+                    setHl("LspReferenceRead", { underdotted = true })
+                    setHl("LspReferenceText", {})
+
+                    -- no undercurls
+                    hlCmd("DiagnosticUnderlineError gui=underline")
+                    hlCmd("DiagnosticUnderlineWarn gui=underline")
+                    hlCmd("DiagnosticUnderlineInfo gui=underline")
+                    hlCmd("DiagnosticUnderlineHint gui=underline")
+                    hlCmd("SpellBad gui=underdotted")
+                    hlCmd("SpellError gui=underdotted")
+                    hlCmd("SpellCap gui=underdotted")
+                    hlCmd("SpellLocal gui=underdotted")
+
+                    -- no overly excessive underlines/bold
+                    hlCmd("ErrorMsg gui=none")
+                    hlCmd("WarningMsg gui=none")
+
+                    -- FIX missing snacks.nvim highlights for trace
+                    setHl("SnacksNotifierTitleTrace", { link = "NotifyTraceTitle" })
+                    setHl("SnacksNotifierTitleIcon", { link = "NotifyTraceIcon" })
+                    setHl("SnacksNotifierTitleBorder", { link = "NotifyTraceBorder" })
+                    setHl("SnacksNotifierTitleFooter", { link = "NotifyTraceBorder" })
+
+                    -- NOTE: lazy.nvim,
+                    setHl("Bold", { bold = true })
+                    setHl("LazyReasonRequire", { link = "@variable.parameter" })
+                end,
             })
-            vim.cmd.colorscheme("gruvbox") -- Comment in order to deactivate "gruvbox"!
+            vim.cmd.colorscheme("gruvbox-material") -- Comment in order to deactivate "gruvbox-material"!
         end,
     },
 }
