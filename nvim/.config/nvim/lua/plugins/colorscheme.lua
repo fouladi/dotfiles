86da9b2eaac9dbb57bc2413fccbf33aca14a032c
Farhad Fouladi <fouladi@gmx.de>
2024-12-26 09:09:26 +0100

    fix: gruvbox setting modified

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
     {
         "sainnhe/gruvbox-material",
+        lazy = false, -- make sure we load this during startup if it is the main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
         opacity = 0.92,
         init = function(spec)
             vim.g.gruvbox_material_background = "medium" -- soft|medium|hard
             vim.g.gruvbox_material_foreground = "original" -- material|mix|original
             vim.g.gruvbox_material_ui_contrast = "low" -- low|high
             vim.g.gruvbox_material_better_performance = 1
+            vim.g.gruvbox_material_enable_italic = 1
             vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
             vim.g.gruvbox_material_inlay_hints_background = "dimmed"
 
