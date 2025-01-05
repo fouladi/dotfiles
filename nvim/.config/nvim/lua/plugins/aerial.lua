4664be68daa51d54224e0d97caec82290ead3cd5
Farhad Fouladi <fouladi@gmx.de>
2025-01-05 09:46:08 +0100

    feat: add new nvim plugin - aerial

---
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,15 @@
+return {
+    "stevearc/aerial.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+    cmd = { "AerialToggle", "AerialOpen", "AerialNext", "AerialPrev" },
+    keys = {
+        { "<leader>a", "<cmd>AerialToggle!<CR>", desc = "[A]erial toggle", mode = "n" },
+        { "<F8>", "<cmd>AerialToggle!<CR>", desc = "[A]erial toggle", mode = "n" },
+        { "<leader>A", "<cmd>AerialNavToggle<CR>", desc = "[A]erial nav toggle", mode = "n" },
+    },
+}
