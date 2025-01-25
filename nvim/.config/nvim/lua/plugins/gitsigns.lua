66904d45d9a4ae49164b67019dcd711a303bf898
Farhad Fouladi <fouladi@gmx.de>
2025-01-25 13:07:26 +0100

    fix: gitsigns conf

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,15 +1,9 @@
 return {
-    {
-        "tpope/vim-fugitive",
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({ signcolumn = false })
-            -- toggle Gitsigns (version control)
-            vim.api.nvim_set_keymap("n", "<leader>gg", ":Gitsigns toggle_signs<CR>", { silent = true })
-            vim.api.nvim_set_keymap("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { silent = true })
-            vim.api.nvim_set_keymap("n", "<leader>gd", ":Gitsigns preview_hunk<CR>", { silent = true })
-        end,
-    },
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup({ signcolumn = false })
+        vim.api.nvim_set_keymap("n", "<leader>gg", ":Gitsigns toggle_signs<CR>", { silent = true })
+        vim.api.nvim_set_keymap("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { silent = true })
+        vim.api.nvim_set_keymap("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { silent = true })
+    end,
 }
--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,7 +1,7 @@
 return {
     "tpope/vim-fugitive", -- A Git wrapper
     config = function()
-        vim.api.nvim_set_keymap("n", "<leader>gv", ":Gdiff<CR>", { silent = true })
+        vim.api.nvim_set_keymap("n", "<leader>gd", ":Gdiff<CR>", { silent = true })
         vim.api.nvim_set_keymap("n", "<leader>gm", ":Gvdiffsplit!<CR>", { silent = true })
     end,
 }
