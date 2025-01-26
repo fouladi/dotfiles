1b71c2e2471f08111b907bca975bc5e661416c32
Farhad Fouladi <fouladi@gmx.de>
2025-01-26 11:33:34 +0100

    fix: silent mode in startin nvim

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -6,19 +6,19 @@ local autocmd = vim.api.nvim_create_autocmd
 vim.g.mapleader = "," -- NOTE: Set leader to comma -  reverse char search
 keymap.set("n", "\\", ",", { noremap = true })
 
-opt.shortmess = "aTI"   -- No -welcome- message
-opt.smartindent = true  -- smart autoindenting when starting a new line
-opt.wrap = false        -- turn-off wrapping - for XML necessary
-opt.undofile = true     -- saves undo history to an undo file
+opt.shortmess = "aTIF" -- No welcome-message, silent
+opt.smartindent = true -- smart autoindenting when starting a new line
+opt.wrap = false -- turn-off wrapping - for XML necessary
+opt.undofile = true -- saves undo history to an undo file
 opt.spelllang = "en,de" -- spelling English and German
-opt.spellsuggest = "5"  -- maximum number of suggestions
-opt.tabstop = 4         -- indentation levels every four columns
-opt.expandtab = true    -- convert all tabs typed to spaces
-opt.shiftwidth = 4      -- indent/outdent by four columns
-opt.shiftround = true   -- indent/outdent to nearest tabstop
-opt.ignorecase = true   -- if search term has at least one capital letter
-opt.smartcase = true    --          then case-insensitiv else case-sensitive
-opt.number = false      -- shows absolute line number on cursor line
+opt.spellsuggest = "5" -- maximum number of suggestions
+opt.tabstop = 4 -- indentation levels every four columns
+opt.expandtab = true -- convert all tabs typed to spaces
+opt.shiftwidth = 4 -- indent/outdent by four columns
+opt.shiftround = true -- indent/outdent to nearest tabstop
+opt.ignorecase = true -- if search term has at least one capital letter
+opt.smartcase = true --          then case-insensitiv else case-sensitive
+opt.number = false -- shows absolute line number on cursor line
 
 -- n: Normal mode.
 -- i: Insert mode.
