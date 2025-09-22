13610c0c1d9dd1e30f817eb76c9ee28b563dde6e
Farhad Fouladi <fouladi@gmx.de>
2025-09-22 12:53:08 +0200

    fix: remove lsp unnecessary parts for nvim lower v0.7.3

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -52,39 +52,6 @@ return {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-
-            -- Neovim v0.7.2 or lower you'll need to create a function
-            -- and add it to the on_attach option of each language server
-            -- NOTE: install each of following tools on your machine and
-            -- Install through Mason
-            -- For Python we use: 'ruff-lsp.lua'
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        hint = {
-                            enable = true,
-                        },
-                    },
-                },
-                capabilities = capabilities,
-            })
-
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-                filetypes = { "go", "gomod", "gowork", "gotmpl" },
-                settings = {
-                    gopls = {
-                        completeUnimported = true,
-                        usePlaceholders = true,
-                        analyses = {
-                            unusedparams = true,
-                        },
-                    },
-                },
-            })
-
             -- Adding 'pyenv virtualenv' for Pyright
             vim.env.PYENV_VERSION = vim.fn.system("pyenv version"):match("(%S+)%s+%(.-%)")
             -- Keymaps for doc, definition, reference and code-action
